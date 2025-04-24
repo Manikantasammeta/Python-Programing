541cfe96763d1c7a00b1e7a09eddfff73cf8039a
Manikantasammeta <manikantasammeta999@gmail.com>
2025-04-24 22:49:12 +0530

    Done with File Configuration

--- ass.py
+++
@@ -1,6 +0,0 @@
-s1="madam"
-s2="mahi"
-print(*[s1[i] for i in range(len(s2)) if s1[i]==s2[i] ])
-
-print(set(s1).intersection(set(s2)))
-
--- l.py
+++
@@ -1,6 +0,0 @@
-print("ghjm")
-n=9
-if n>5:
-    print("n is grater")
-else
-print("n is amsller")
