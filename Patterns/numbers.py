7f34e7cdb1f67be8722de277a65f162cd80b1c61
Manikantasammeta <manikantasammeta999@gmail.com>
2025-07-10 22:52:53 +0530

    Done

--- Patterns/numbers.py
+++ Patterns/numbers.py
@@ -1,8 +1,15 @@
-n=5
-for i in range(n):
-    for j in range(n):
-        if j==n//2 or i==n-1 or j==n//2-i:
-            print("*",end=" ")
-        else:
-            print(" ",end=" ")
-    print()
+# n=5
+# for i in range(n):
+#     for j in range(n):
+#         if j==n//2 or i==n-1 or j==n//2-i:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()
+
+# n=5
+#     *     
+#   * *     
+# *   *     
+#     *     
+# * * * * * 
