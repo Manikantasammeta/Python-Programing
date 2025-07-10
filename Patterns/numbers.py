e795242f530fc08392cfe580df49e0f47b108920
Manikantasammeta <manikantasammeta999@gmail.com>
2025-07-10 23:11:56 +0530

    done

--- Patterns/numbers.py
+++ Patterns/numbers.py
@@ -69,3 +69,23 @@ for i in range(n):
 # * * * * * * * 
 #         *     
 #         *     
+
+
+n=7
+for i in range(n):
+    for j in range(n):
+        if i==0 or (j==0 and i<=n//2) or i==n//2 or (j==n-1 and i>n//2) or i==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+    
+    
+# n=7
+# * * * * * * * 
+# *             
+# *             
+# * * * * * * * 
+#             * 
+#             * 
+# * * * * * * * 
