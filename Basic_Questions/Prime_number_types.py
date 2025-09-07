5b2640f096bcad81dffaf1eba4c7e7d33452158f
Manikantasammeta <manikantasammeta999@gmail.com>
2025-09-07 13:20:30 +0530

    new logic is uploded

--- Basic_Questions/Prime_number_types.py
+++ Basic_Questions/Prime_number_types.py
@@ -28,5 +28,12 @@ else:
 
 # By useing functions 
 
+def check_prime_(n):
+    for i in range(2,(n//2)+1):
+        if n%i==0:
+            return False
+    else:
+        return True
+
     
     
