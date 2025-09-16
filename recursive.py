928e37c66ee815f433b5081926a11127997052b3
Manikantasammeta <manikantasammeta999@gmail.com>
2025-09-16 17:44:14 +0530

    new logic is uploded

--- recursive.py
+++ recursive.py
@@ -186,3 +186,12 @@
 #     return l
 # print(fun(x))
             
+
+def feb_series(n,a=0,b=1):
+    if a==0:
+        print(a,b,end=" ")
+    if n!=2:
+        c=a+b
+        print(c,end=" ")
+        feb_series(a=b,b=c,n=n-1)
+feb_series(7)
