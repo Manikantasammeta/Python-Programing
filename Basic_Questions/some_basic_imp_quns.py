24e12bd4422b108284b85ea3e00d08b310706369
Manikantasammeta <manikantasammeta999@gmail.com>
2025-10-09 21:02:05 +0530

    new logic is uploded

--- Basic_Questions/some_basic_imp_quns.py
+++ Basic_Questions/some_basic_imp_quns.py
@@ -7,13 +7,3 @@ print(fact)
 
 
 # The Factor of the given number is prime or not 
-n=5
-fact=1
-for i in range(1,n+1):
-    fact*=i
-for j in range(2,fact//2):
-    if fact%j==0:
-        print(fact,"is not  a prime number ")
-        break
-else:
-    print(fact,"is a prime number ")
