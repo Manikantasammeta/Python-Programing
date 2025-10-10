fe8a7f5dc048cee537661e03ed4b260b6bc1c581
Manikantasammeta <manikantasammeta999@gmail.com>
2025-10-10 11:38:13 +0530

    new logic is uploded in basic Questions .py

--- Basic_Questions/some_basic_imp_quns.py
+++ Basic_Questions/some_basic_imp_quns.py
@@ -7,3 +7,12 @@ print(fact)
 
 
 # The Factor of the given number is prime or not 
+n=5
+fact=1
+for i in range(1,n+1):
+    fact*=i
+for j in range(2,fact):
+    if fact%j==0:
+        print(fact,"is not a Prime number")
+else:
+    print(fact," is a prime number ")
