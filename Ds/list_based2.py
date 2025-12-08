b5c9475d09582b6db79774f564899ae7e126a245
Manikantasammeta <manikantasammeta999@gmail.com>
2025-12-08 21:21:44 +0530

    New logic is uploded

--- Ds/list_based2.py
+++ Ds/list_based2.py
@@ -7,6 +7,7 @@
 
 #Output ['R:rama', 'R:raju', 'R:raghu', 'M:mani', 'S:sita']   #convertintg the list into list
 
+
 input_list=[[1,2],[3,4],[5,6],[7,8]]
 list_comp=[j  for i in input_list for j in i]
 print(list_comp)  #[1, 2, 3, 4, 5, 6, 7, 8]
